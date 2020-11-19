ed01d7e923c26b6a5547bdf7327ab41b17e36f63
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-11-19 08:56:49 +0000

    Install, configure and test Webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [{
+        loader: 'style-loader',
+      },
+      {
+        loader: 'css-loader',
+      },
+      ],
+    },
+    {
+      test: /\.(png|jpeg|jpg|svg|gif)$/,
+      use: [{
+        loader: 'file-loader',
+      }],
+    },
+    ],
+  },
+};
